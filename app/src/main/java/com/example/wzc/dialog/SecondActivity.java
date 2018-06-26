56e2e292c7587ca643f26bc4924c810b016c08d3
wylovechip <wuying-2015s@outlook.com>
2018-06-26 13:37:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/wzc/dialog/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.wzc.dialog;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lenovo on 2018/4/16.
+ */
+
+
+
+
+public class SecondActivity extends AppCompatActivity {
+}
+
